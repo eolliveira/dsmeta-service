a2601fa6fa2d184688fc11bcab30b2f225e96cbc
Erick Oliveira <erick.oliveira9607@gmail.com>
2022-09-09 00:34:50 -0300

    Initial commit

---
+++ src/main/java/com/example/dsmetaservice/DsmetaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.dsmetaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmetaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmetaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dsmetaservice/DsmetaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dsmetaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
